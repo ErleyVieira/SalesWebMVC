125996f54cac75edd362f1791e3ab7104afa819b
Erley Dantas <erley_dantas@hotmail.com>
2022-11-19 12:52:49 -0300

    Criação da tela de login

---
+++ SalesWebMVC/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using SalesWebMVC.Models.LoginData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            Login login = new();
+
+            login.email = "dantaserley@gmail.com";
+            login.senha = "Soulevans1#";
+
+            return View(login);
+        }
+    }
+}
