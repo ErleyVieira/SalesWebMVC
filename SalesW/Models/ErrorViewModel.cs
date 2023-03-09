e9c109019cb82f4290a3ef36d2b8cb77ea38a350
Erley Vieira <erley.vieira@outlook.com.br>
2023-03-08 21:26:17 -0300

    Adicionar arquivos de projeto.

---
+++ SalesW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
