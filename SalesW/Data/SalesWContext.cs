05ca975d2590285f713dbdaae73a2fecc2678fc0
Erley Vieira <erley.vieira@outlook.com.br>
2023-03-13 21:32:20 -0300

    Adicionando o CRUD scafollding ao projeto

---
+++ SalesW/Data/SalesWContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesW.Models;
+
+namespace SalesW.Data
+{
+    public class SalesWContext : DbContext
+    {
+        public SalesWContext (DbContextOptions<SalesWContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesW.Models.Department> Department { get; set; } = default!;
+    }
+}
--- SalesW/Program.cs
+++ SalesW/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesW.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SalesWContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWContext") ?? throw new InvalidOperationException("Connection string 'SalesWContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
